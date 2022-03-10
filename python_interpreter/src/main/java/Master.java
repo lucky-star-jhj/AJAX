167cd333f7db78e3ce8479b2993f04f496ec5d10
JiangHangjun <2690759382@qq.com>
2022-03-10 10:09:06 +0800

    master commit 1

--- python_interpreter/src/main/java/Master.java
+++ python_interpreter/src/main/java/Master.java
@@ -1,5 +1,6 @@
 public class Master {
     public static void main(String[] args) {
         System.out.println("Hello This is Master !");
+        System.out.println("Er...It is my error !");
     }
 }

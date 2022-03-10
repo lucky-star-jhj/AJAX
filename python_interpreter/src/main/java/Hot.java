5e9fede22cdee9d5772b654f72c7e6ea14b78807
JiangHangjun <2690759382@qq.com>
2022-03-10 11:00:58 +0800

    master commit 4

--- python_interpreter/src/main/java/Hot.java
+++ python_interpreter/src/main/java/Hot.java
@@ -4,5 +4,6 @@ public class Hot {
         System.out.println("master come on");
         System.out.println("No, It is mine ! You get out !");
         System.out.println("I want to share codes with GitHub !");
+        System.out.println("I will pull next");
     }
 }

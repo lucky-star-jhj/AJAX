e55334fbbb9e1cde216bbb21bd7c6005e95e569c
JiangHangjun <2690759382@qq.com>
2022-03-10 09:48:30 +0800

    my 3 commit

--- python_interpreter/src/main/java/javaRunPython.java
+++ python_interpreter/src/main/java/javaRunPython.java
@@ -15,6 +15,9 @@ public class javaRunPython {
                 String.valueOf(5),String.valueOf(10),String.valueOf(15),String.valueOf(15),String.valueOf(20)};
         proc=Runtime.getRuntime().exec(args1);
         System.out.println("Hello my 2 commit ! ");
+        System.out.println("Hello my 3 commit ! ");
+
+
         //这里注意乱码
         BufferedReader in =new BufferedReader(new InputStreamReader(proc.getInputStream(),"GBK"));
         String line = null;

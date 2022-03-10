215ca9281a5df6645e59034efb5f536580e5b337
Hangjun Jiang <82208323+lucky-star-jhj@users.noreply.github.com>
2022-03-10 11:05:31 +0800

    Update Hot.java

--- python_interpreter/src/main/java/Hot.java
+++ python_interpreter/src/main/java/Hot.java
@@ -5,5 +5,6 @@ public class Hot {
         System.out.println("No, It is mine ! You get out !");
         System.out.println("I want to share codes with GitHub !");
         System.out.println("I will pull next");
+		System.out.println("Please pull the lastest version into idea");
     }
 }
